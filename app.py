4136f87e74ec6e6d2cb3800e5e9f91b64b5ebc4c
André Amorim <114627100+Baldros@users.noreply.github.com>
2025-07-30 13:11:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
+from textwrap import dedent
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.googlesearch import GoogleSearchTools
 
 st.write("Hello World")
